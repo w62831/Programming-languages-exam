067095ca4464416c6f835aacab3bcdf34818e6c1
w62831 <72877017+w62831@users.noreply.github.com>
2021-02-10 21:20:42 +0000

    Add files via upload

---
+++ 1st task/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace expences_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readButton = new System.Windows.Forms.Button();
+            this.task1 = new System.Windows.Forms.Button();
+            this.task3 = new System.Windows.Forms.Button();
+            this.task2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.readTextBox = new System.Windows.Forms.TextBox();
+            this.showTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(12, 58);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(95, 32);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "read the file";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // task1
+            // 
+            this.task1.Location = new System.Drawing.Point(113, 58);
+            this.task1.Name = "task1";
+            this.task1.Size = new System.Drawing.Size(75, 32);
+            this.task1.TabIndex = 1;
+            this.task1.Text = "1 task";
+            this.task1.UseVisualStyleBackColor = true;
+            this.task1.Click += new System.EventHandler(this.task1_Click);
+            // 
+            // task3
+            // 
+            this.task3.Location = new System.Drawing.Point(281, 58);
+            this.task3.Name = "task3";
+            this.task3.Size = new System.Drawing.Size(75, 32);
+            this.task3.TabIndex = 2;
+            this.task3.Text = "3 task";
+            this.task3.UseVisualStyleBackColor = true;
+            this.task3.Click += new System.EventHandler(this.task3_Click);
+            // 
+            // task2
+            // 
+            this.task2.Location = new System.Drawing.Point(194, 58);
+            this.task2.Name = "task2";
+            this.task2.Size = new System.Drawing.Size(75, 32);
+            this.task2.TabIndex = 3;
+            this.task2.Text = "2 task";
+            this.task2.UseVisualStyleBackColor = true;
+            this.task2.Click += new System.EventHandler(this.task2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Provide the path";
+            // 
+            // readTextBox
+            // 
+            this.readTextBox.Location = new System.Drawing.Point(12, 29);
+            this.readTextBox.Name = "readTextBox";
+            this.readTextBox.Size = new System.Drawing.Size(776, 22);
+            this.readTextBox.TabIndex = 5;
+            // 
+            // showTextBox
+            // 
+            this.showTextBox.Location = new System.Drawing.Point(12, 96);
+            this.showTextBox.Multiline = true;
+            this.showTextBox.Name = "showTextBox";
+            this.showTextBox.Size = new System.Drawing.Size(776, 342);
+            this.showTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showTextBox);
+            this.Controls.Add(this.readTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.task2);
+            this.Controls.Add(this.task3);
+            this.Controls.Add(this.task1);
+            this.Controls.Add(this.readButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button task1;
+        private System.Windows.Forms.Button task3;
+        private System.Windows.Forms.Button task2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox readTextBox;
+        private System.Windows.Forms.TextBox showTextBox;
+    }
+}
+
---
+++ 1st task/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace expences_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            var path = readTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File doesn't exist. Cannot proceed");
+                return;
+            }
+
+            showTextBox.Text = File.ReadAllText(path);
+        }
+
+        private void task1_Click(object sender, EventArgs e)
+        {
+            var path = readTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File doesn't exist. Cannot proceed");
+                return;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+
+            var expences = new List<Expences>();
+            foreach (var line in lines)
+            {
+                var split = line.Split('|');
+                var date = DateTime.ParseExact(split[0], "yyyy-MM-dd", null);
+                var price = Convert.ToDecimal(split[1]);
+                var category = split[2];
+
+                var expence = new Expences(date, price, category);
+                expences.Add(expence);
+            }
+
+            var catnum = expences
+                .Select(x => x.Category).Distinct().Count();
+
+            var total = expences
+                .Select(x => x.Price).Sum();
+
+            showTextBox.Text += $"The number of categories: {catnum}, total expences = {total}";
+        }
+
+        private void task2_Click(object sender, EventArgs e)
+        {
+            var path = readTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File doesn't exist. Cannot proceed");
+                return;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+
+            var expences = new List<Expences>();
+            foreach (var line in lines)
+            {
+                var split = line.Split('|');
+                var date = DateTime.ParseExact(split[0], "yyyy-MM-dd", null);
+                var price = Convert.ToDecimal(split[1]);
+                var category = split[2];
+
+                var expence = new Expences(date, price, category);
+                expences.Add(expence);
+            }
+
+            var catnum = expences
+                .Select(x => x.Category).Distinct().Count();
+
+            var total = expences
+                .Select(x => x.Price).Sum();
+
+            IEnumerable<string> cats = expences
+                .Select(x => x.Category).Distinct();
+
+            IEnumerable<DateTime> dates = expences
+                .Select(x => x.DateTime).Distinct();
+
+            showTextBox.Text += $"\r\nNumber of categories: {catnum}, total expences = {total}";
+            showTextBox.Text += $"\r\nCategories: ";
+            foreach (var cat in cats)
+                showTextBox.Text += $"{cat}, ";
+            showTextBox.Text += $"\r\nDates: ";
+            foreach (var dat in dates)
+                showTextBox.Text += $"{dat.ToString("yyyy-MM-dd")}, ";
+        }
+
+        private void task3_Click(object sender, EventArgs e)
+        {
+            var path = readTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File doesn't exist. Cannot proceed");
+                return;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+
+            var expences = new List<Expences>();
+            foreach (var line in lines)
+            {
+                var split = line.Split('|');
+                var date = DateTime.ParseExact(split[0], "yyyy-MM-dd", null);
+                var price = Convert.ToDecimal(split[1]);
+                var category = split[2];
+
+                var expence = new Expences(date, price, category);
+                expences.Add(expence);
+            }
+
+            var catnum = expences
+                .Select(x => x.Category).Distinct().Count();
+
+            var total = expences
+                .Select(x => x.Price).Sum();
+
+            IEnumerable<string> cats = expences
+                .Select(x => x.Category).Distinct();
+
+            var excat = expences
+                .GroupBy(x => x.Category)
+                .Select(x => $"{x.Key}: {x.Sum(y => y.Price)}");
+
+            showTextBox.Text = string.Join("\r\n", excat);
+
+            showTextBox.Text += $"\r\nNumber of categories: {catnum}, total expences = {total}";
+
+            showTextBox.Text += $"\r\nCategories: ";
+            foreach (var cat in cats)
+                showTextBox.Text += $"{cat}, ";
+        }
+
+
+    }
+    public class Expences
+    {
+        public DateTime DateTime { get; }
+        public decimal Price { get; }
+        public string Category { get; }
+
+        public Expences(DateTime dateTime, decimal price, string category)
+        {
+            DateTime = dateTime;
+            Price = price;
+            Category = category;
+        }
+    }
+}
+
